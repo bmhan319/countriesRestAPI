992fc2bf20574f8a73fd291ebe7f4ae24eaa23d2
Brian Han <bmhan319@hotmail.com>
2020-10-26 21:10:33 -0700

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>hiq</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
