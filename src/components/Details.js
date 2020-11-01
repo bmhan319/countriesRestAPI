d28d5fc14406bbf99038e27d71cb36a4e9e13035
Brian Han <bmhan319@hotmail.com>
2020-10-31 18:16:37 -0700

    created desktop styling for details

--- src/components/Details.js
+++ src/components/Details.js
@@ -33,7 +33,7 @@ class Details extends Component {
   render() {
     const country = this.props.location.countryData
     return (
-      <div id="detailsContainer" className="darkBg">
+      <div id="detailsContainer" className="detailsContainer darkBg">
 
         <div className="backButtonContainer">
           <NavLink className="backButtonLink" to='/'>
@@ -41,35 +41,46 @@ class Details extends Component {
           </NavLink>
         </div>
 
-        <div className="detailsFlagContainer">
-          <img id="detailsFlagImg" className="detailsFlagImg darkBoxShadow" src={country.flag} alt={country.flag} />
-        </div>
-
-        <div id="detailsCountryInfoContainer" className="detailsCountryInfoContainer darkText">
-          <h2 className="detailsCountryName">{country.name}</h2>
-          <p className="detailsCountryText"><span className="detailsCountrySubject">Native Name: </span>{ (country.nativeName).toLocaleString() }</p>
-          <p className="detailsCountryText"><span className="countrySubject">Population: </span>{ (country.population).toLocaleString() }</p>
-          <p className="detailsCountryText"><span className="detailsCountrySubject">Region: </span>{country.region}</p>
-          <p className="detailsCountryText"><span className="detailsCountrySubject">Sub Region: </span>{country.subregion}</p>
-          <p className="detailsCountryText detailsCapital"><span className="detailsCountrySubject">Capital: </span>{country.capital}</p>
-          <br/>
-          <p className="detailsCountryText"><span className="detailsCountrySubject">Top Level Domain: </span>{country.topLevelDomain}</p>
-          <p className="detailsCountryText"><span className="detailsCountrySubject">Currencies: </span>{country.currencies[0].name}</p>
-          <p className="detailsCountryText detailsLanguages"><span className="detailsCountrySubject">Languages: </span>{this.getLanguages()}</p>
-          
-            <h3 className="detailsBorderName">Border Countries:</h3>
-          <div className="detailsBorderContainer">
-            {this.addBorderCountries().map( (border,ind) => (
-              <NavLink className="countryLink" key={ind} to={{
-                pathname: '/details',
-                countryData: border[1],
-                state: this.props.location.state
-              }}>
-                <div className="borderCountry darkElem darkText darkBoxShadow">{border[0]}</div>
-              </NavLink>
-            ))}
+        <div className="countryDataContainer">
+          <div className="detailsFlagContainer">
+            <img id="detailsFlagImg" className="detailsFlagImg darkBoxShadow" src={country.flag} alt={country.flag} />
           </div>
-        </div>
+
+          <div id="detailsCountryInfoContainer" className="detailsCountryInfoContainer darkText">
+            <h2 className="detailsCountryName">{country.name}</h2>
+
+            <div className="detailsTextContainer">
+              <div className="detailsTextCol1">
+                <p className="detailsCountryText"><span className="detailsCountrySubject">Native Name: </span>{ (country.nativeName).toLocaleString() }</p>
+                <p className="detailsCountryText"><span className="countrySubject">Population: </span>{ (country.population).toLocaleString() }</p>
+                <p className="detailsCountryText"><span className="detailsCountrySubject">Region: </span>{country.region}</p>
+                <p className="detailsCountryText"><span className="detailsCountrySubject">Sub Region: </span>{country.subregion}</p>
+                <p className="detailsCountryText detailsCapital"><span className="detailsCountrySubject">Capital: </span>{country.capital}</p>
+                <br/>
+              </div>
+              <div className="detailsTextCol2">
+                <p className="detailsCountryText"><span className="detailsCountrySubject">Top Level Domain: </span>{country.topLevelDomain}</p>
+                <p className="detailsCountryText"><span className="detailsCountrySubject">Currencies: </span>{country.currencies[0].name}</p>
+                <p className="detailsCountryText detailsLanguages"><span className="detailsCountrySubject">Languages: </span>{this.getLanguages()}</p>
+              </div>
+            </div>
+
+            <div className="detailsBorderWrapper">
+              <h3 className="detailsBorderName">Border Countries:</h3>
+              <div className="detailsBorderContainer">
+                {this.addBorderCountries().map( (border,ind) => (
+                  <NavLink className="countryLink" key={ind} to={{
+                    pathname: '/details',
+                    countryData: border[1],
+                    state: this.props.location.state
+                  }}>
+                    <div className="borderCountry darkElem darkText darkBoxShadow">{border[0]}</div>
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>  
 
       </div>
     )
